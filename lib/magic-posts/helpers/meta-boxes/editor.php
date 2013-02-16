72f779486b805022b31bec7e97ccf822637cbb10
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2013-02-16 21:37:20 -0200

    wp-editor-container

--- lib/magic-posts/helpers/meta-boxes/editor.php
+++ lib/magic-posts/helpers/meta-boxes/editor.php
@@ -1,1 +1,7 @@
-<?php wp_editor($field_value, $field_name); ?>
+<div class="magic-posts-meta-box">
+<?php wp_editor($field_value, $field_name, array(
+  'editor_css' => '<style type="text/css">
+    .magic-posts-meta-box .wp-editor-container { background-color:#FFF; }
+  </style>'
+)); ?>
+</div>
