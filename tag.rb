1485f06dbd09bd1c9bdb738a42624d71d2af867a
Guilherme Baptista <guilhermebaptistasilva@gmail.com>
2013-02-19 00:23:59 -0300

    tag scripts

---
+++ tag.rb
@@ -0,0 +1,59 @@
+# encoding: utf-8
+
+require 'fileutils'
+
+# Use:
+# ruby tag.rb 0.0.5 tag
+# ruby tag.rb 0.0.5 svn
+# ruby tag.rb 0.0.5 git
+
+trunk = Dir.pwd
+tag   = trunk.gsub(/trunk$/, 'tags/'+ARGV.first)
+
+if ARGV[1] == 'git'
+
+  puts "git tag -a #{ARGV.first} -m \"Magic Posts #{ARGV.first}\""
+
+elsif ARGV[1] == 'svn'
+
+  puts "svn add tags/#{ARGV.first}/*"
+  puts "svn ci -m \"tagging version #{ARGV.first}\""
+
+elsif ARGV[1] == 'tag'
+
+  if Dir.exists? tag
+    puts 'tag exists!'
+    abort
+  end
+
+  FileUtils.mkdir tag
+  FileUtils.cp_r Dir.glob('*'), tag
+
+  [
+    tag + '/css/.svn',
+    tag + '/js/.svn',
+    tag + '/lib/.svn',
+    tag + '/lib/magic-posts/.svn',
+    tag + '/lib/magic-posts/helpers/.svn',
+    tag + '/lib/magic-posts/helpers/.svn',
+    tag + '/lib/magic-posts/helpers/meta-boxes/.svn',
+    tag + '/lib/magic-posts/inflector/.svn',
+    tag + '/lib/magic-posts/settings/.svn',
+    tag + '/test/.svn',
+    tag + '/.git',
+    tag + '/.svn',
+    tag + '/README.md',
+    tag + '/svn_tag.rb',
+    tag + '/svn_tag.sh',
+    tag + '/.gitignore',
+  ].each do |remove|
+    if File.exists? remove
+      if File.directory?(remove)
+        FileUtils.rm_rf remove
+      else
+        File.delete remove
+      end
+    end
+  end
+
+end
---
+++ tag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Use:
+# cd /media/gbaptista/server/projects/svn/wordpress/magic-posts/
+# bash trunk/tag.sh 0.0.5
+
+svn=$PWD
+trunk="$PWD/trunk"
+#trunk="/media/gbaptista/server/projects/$1"
+
+cd $trunk
+ruby tag.rb $1 tag
+ruby tag.rb $1 git
+cd $svn
+ruby trunk/tag.rb $1 svn
